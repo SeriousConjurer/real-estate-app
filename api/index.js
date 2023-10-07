5e80f454dd4ec9e7b112d737d27d9353d2938cb7
Himanshu Yadav <himanshuinsta1818@gmail.com>
2023-10-08 00:25:28 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-const express = require('express')
 
-const app = express()
-app.listen(3000 , () =>{
-    console.log("sever listening to port 3000")
+
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+ // we cannot use environmental variables(.env) by default , we use dotenv to access them
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected")
+}).catch((err)=>{
+    console.log(err)
 })
+
+const app = express();
+app.listen(3000, () => {
+  console.log("sever listening to port 3000");
+});
