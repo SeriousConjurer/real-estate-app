2f8cdd91d9d937941b9b1558a50f85a8f67412fe
Himanshu Yadav <himanshuinsta1818@gmail.com>
2023-10-11 01:42:08 +0530

    added update user functionality

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js"
+import cookieParser from "cookie-parser";
 // we cannot use environmental variables(.env) by default , we use dotenv to access them
 dotenv.config();
 
@@ -18,8 +20,10 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log("sever listening to port 3000");
